dacf062ffc05b9da3a18694ade089992ffcb5a84
Julio Gabriel <julio.otoni@outlook.com>
2023-05-17 12:37:48 -0300

    Update 17-05

---
+++ ControleDe Contatos/ControleDe Contatos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ControleDe_Contatos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
